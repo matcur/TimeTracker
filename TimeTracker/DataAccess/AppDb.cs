332447b8aa8fc3294a77ea91a14c7ce3656e39fd
matsur <ru.far2014@gmail.com>
2021-11-30 22:08:51 +0500

    ref: remove Database.EnsureCreated

--- TimeTracker/DataAccess/AppDb.cs
+++ TimeTracker/DataAccess/AppDb.cs
@@ -13,7 +13,6 @@ namespace TimeTracker.DataAccess
 
         public AppDb(DbContextOptions options): base(options)
         {
-            Database.EnsureCreated();
         }
         
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
